f932a3619080d24a3218525a7f1b6161af055aff
Alexei Ignatiev <ignatiev1551@mail.ru>
2025-01-08 20:09:09 +0300

    Adding configuration for deploing in prod

--- dagster_university/project.py
+++ dagster_university/project.py
@@ -1,8 +1,10 @@
 from pathlib import Path
+import os
 
 from dagster_dbt import DbtProject
 
 dbt_project = DbtProject(
     project_dir=Path(__file__).joinpath("..","..", "analytics").resolve(),
+    target=os.getenv("DBT_TARGET")
 )
 dbt_project.prepare_if_dev()
